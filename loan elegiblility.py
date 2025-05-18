2e9955f5b10890bda8be0cf46cace11dff40a09d
Subh130 <subhchaudhary541@gmail.com>
2025-05-18 16:30:58 +0545

    c1

--- loan elegiblility.py
+++ loan elegiblility.py
@@ -1,3 +1,7 @@
+# Be at least 21 years old, OR a student.
+# Have an income of at least $25,000, OR a credit score of 700 or more.
+# If the person is a student and under 21, they must have a credit score over 750 to be eligible.
+# Anyone under 18 is automatically not eligible.
 print("Checking eligibility for taking loan")
 
 age = int(input("enter your age: "))
@@ -17,7 +21,7 @@ if age >=18:
         if student.ucase() == "YES":
             if salary >= 25000:
                 print("Eligible for loan")
-            elif credit >= 700:
+            elif credit >= 750:
                 print("Eligible for loan")
             else:
                 print("Not eligible for loan")
