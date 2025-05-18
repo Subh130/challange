69c04c31f75cc88a807d847c198c15f72fbb681e
Subh130 <subhchaudhary541@gmail.com>
2025-05-18 15:40:20 +0545

    C1

--- loan elegiblility.py
+++ loan elegiblility.py
@@ -1,12 +1,12 @@
-print("Checking elegibility for taking loan")
+print("Checking eligibility for taking loan")
 
 age = int(input("enter your age: "))
-student = input("are you a sturent (Yes/No): ")
+student = input("are you a student (Yes/No): ")
 salary = int(input("what is your monthly salary: "))
 credit = int(input("enter your credit score: "))
 
 if age >=18:
-    if age <=21: 
+    if age >= 21: 
         if salary >= 25000:
             print("Eligible for loan")
         elif credit >= 700:
