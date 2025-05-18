3d09461cce8e0645f1e22b2e1cc82ab2e7c33930
Subh130 <subhchaudhary541@gmail.com>
2025-05-18 14:36:32 +0545

    first commit

---
+++ loan elegiblility.py
@@ -0,0 +1,21 @@
+print("Checking elegibility for taking loan")
+
+age = int(input("enter your age: "))
+student = input("are you a sturent (Yes/No): ")
+salary = int(input("what is your monthly salary: "))
+credit = int(input("enter your credit score: "))
+
+if age >=18:
+    if age <=21: 
+        if salary >= 25000:
+            print("Eligible for loan")
+        elif credit >= 700:
+                print("Eligible for loan")
+    elif age < 21: 
+        if student.ucase() == "YES":
+            if salary >= 25000:
+                print("Eligible for loan")
+            elif credit >= 700:
+                print("Eligible for loan")
+else: 
+    print("Not eligible for loan")                
